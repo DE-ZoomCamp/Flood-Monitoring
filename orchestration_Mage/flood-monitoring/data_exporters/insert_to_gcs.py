310c72e5ca946c51ec31492e633d4b756e149cc0
Wanjohi Christopher <55980747+WanjohiChristopher@users.noreply.github.com>
2024-04-19 11:27:08 +0300

    Update insert_to_gcs.py

--- orchestration_Mage/flood-monitoring/data_exporters/insert_to_gcs.py
+++ orchestration_Mage/flood-monitoring/data_exporters/insert_to_gcs.py
@@ -33,33 +33,3 @@ def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
     # Export the combined DataFrame to GCS
     gcs.export(combined_df, bucket_name, object_key)
 
-
-
-# from mage_ai.settings.repo import get_repo_path
-# from mage_ai.io.config import ConfigFileLoader
-# from mage_ai.io.google_cloud_storage import GoogleCloudStorage
-# from pandas import DataFrame
-# from os import path
-
-# if 'data_exporter' not in globals():
-#     from mage_ai.data_preparation.decorators import data_exporter
-
-
-# @data_exporter
-# def export_data_to_google_cloud_storage(df: DataFrame, **kwargs) -> None:
-#     """
-#     configuration settings in 'io_config.yaml'.
-
-#     """
-    
-#     config_path = path.join(get_repo_path(), 'io_config.yaml')
-#     config_profile = 'default'
-
-#     bucket_name = 'flood-bucket24'
-#     object_key = 'floods.csv'
-
-#     GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
-#         df,
-#         bucket_name,
-#         object_key,
-#     )
