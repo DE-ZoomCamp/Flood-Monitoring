4f781713a0942c2f9e0da6f01ae2d571daba05ae
Wanjohi Christopher <55980747+WanjohiChristopher@users.noreply.github.com>
2024-04-19 11:26:24 +0300

    Update extract_py.py

--- orchestration_Mage/flood-monitoring/data_loaders/extract_py.py
+++ orchestration_Mage/flood-monitoring/data_loaders/extract_py.py
@@ -41,32 +41,4 @@ def test_output(output, *args) -> None:
     assert isinstance(output, pd.DataFrame), 'Output should be a pandas DataFrame'
     assert len(output) > 0, 'The DataFrame should have at least one row'
 
-# import io
-# import pandas as pd
-# import requests
-# if 'data_loader' not in globals():
-#     from mage_ai.data_preparation.decorators import data_loader
-# if 'test' not in globals():
-#     from mage_ai.data_preparation.decorators import test
 
-
-# @data_loader
-# def load_data_from_api(*args, **kwargs):
-#     """
-#     loading data from floods API
-#     """
-#     url = 'http://environment.data.gov.uk/flood-monitoring/id/floods.json'
-    
-#     response = requests.get(url)
-#     data = response.json()
-#     items = data['items']
-#     df = pd.DataFrame(items)
-
-#     return df
-
-# @test
-# def test_output(output, *args) -> None:
-#     """
-#     Template code for testing the output of the block.
-#     """
-#     assert output is not None, 'The output is undefined'
